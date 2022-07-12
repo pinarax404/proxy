2a180562cb743d86aa072192596b7da68e019094
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2022-07-12 21:09:46 +0700

    Create list.js

---
+++ list.js
@@ -0,0 +1,17 @@
+var auth = {
+    "user":"iwy8eb6f-va3pd47",
+    "pass":"du5svn7b8m"
+};
+
+var server = [
+    "ca-042.totallyacdn.com",
+    "ca-036.totallyacdn.com",
+    "ca-040.totallyacdn.com",
+    "ca-031.totallyacdn.com",
+    "ca-023.totallyacdn.com",
+    "ca-024.totallyacdn.com",
+    "ca-037.totallyacdn.com",
+    "ca-west-018.totallyacdn.com",
+    "ca-west-015.totallyacdn.com",
+    "ca-west-007.totallyacdn.com"
+];
